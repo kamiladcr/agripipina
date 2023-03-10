ab4a182d4040ed75784028e57bf768c25c8bf9f1
Kamila Dzhavatova <kamila.dzhavatova@gmail.com>
2023-03-10 18:56:30 +0100

    add masks

--- bot.py
+++ bot.py
@@ -6,30 +6,34 @@ bot = TeleBot("6139726981:AAHYXuNByfotN4RFPkIoepNKrSaaSvyJJMg", parse_mode=None)
 
 @bot.message_handler(commands=["help"])
 def help(message):
-	bot.reply_to(message, "hei, handsome. this bot pins music links...")
+	bot.reply_to(
+		message,
+		"hi. \nthis bot pins links so you can check them out later. \nuse the command /pin to specify which links you would like to pin \ne.g. /pin spotify soundcloud songwhip",
+	)
 
 
 @bot.message_handler(commands=["pin"])
-def what_links(message: Message):
-	text = "what would you like to pin?"
+def get_masks(message: Message):
+	input = message.text if message.text else ""
+	masks = input.split()[1:]
+	print(f"masks: {masks}")
+	text = "emply list" if message.text == "/pin" else "ok"
 	sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
-	bot.register_next_step_handler(sent_msg, to_pin)
+	bot.register_next_step_handler(sent_msg, process_masks)
+	print(f"sent_msg: {sent_msg}")
+	return message.text
 
 
-# @bot.message_handler(commands=["pin"])
-# def send_welcome(message: Message):
-#	bot.reply_to(message, "what to pin")
-
-
-def to_pin(message: Message) -> bool:
+def process_masks(message: Message) -> bool:
 	entities = message.entities if message.entities else []
 	for entity in entities:
+		print(f"entity type: {entity.type}")
 		if entity.type == "url":
 			return True
 	return False
 
 
-@bot.message_handler(func=to_pin)
+@bot.message_handler(func=process_masks)
 def pin_message(message):
 	bot.pin_chat_message(
 		chat_id=message.chat.id,
